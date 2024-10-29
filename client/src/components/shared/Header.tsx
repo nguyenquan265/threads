739fdda56dfbb50d0fc6fecfd0280a189b56ae1e
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-10-29 19:00:32 +0700

    add signout button and user profile button

--- client/src/components/shared/Header.tsx
+++ client/src/components/shared/Header.tsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom'
 import Logo from '@/assets/logo.svg'
+import LogOut from '@/assets/logout.svg'
+import { OrganizationSwitcher, SignOutButton, SignedIn } from '@clerk/clerk-react'
 
 const Header = () => {
   return (
@@ -8,6 +10,26 @@ const Header = () => {
         <img src={Logo} alt='logo' width={28} height={28} />
         <p className='text-heading3-bold text-light-1 max-xs:hidden'>Threads</p>
       </Link>
+
+      <div className='flex items-center gap-1'>
+        <div className='block md:hidden'>
+          <SignedIn>
+            <SignOutButton>
+              <div className='flex cursor-pointer'>
+                <img src={LogOut} alt='logout' width={24} height={24} />
+              </div>
+            </SignOutButton>
+          </SignedIn>
+        </div>
+
+        <OrganizationSwitcher
+          appearance={{
+            elements: {
+              organizationSwitcherTrigger: 'py-2 px-4'
+            }
+          }}
+        />
+      </div>
     </nav>
   )
 }
