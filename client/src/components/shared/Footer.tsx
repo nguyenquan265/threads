72cdc752987b645a68a00d279584580811ff92d2
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-10-29 19:36:51 +0700

    complete Footer

--- client/src/components/shared/Footer.tsx
+++ client/src/components/shared/Footer.tsx
@@ -1,5 +1,27 @@
+import { sidebarLinks } from '@/utils/constants'
+import { Link, useLocation } from 'react-router-dom'
+
+// Use as mobile navigation
 const Footer = () => {
-  return <div>Footer</div>
+  const location = useLocation()
+
+  return (
+    <footer className='bottombar'>
+      <div className='bottombar_container'>
+        {sidebarLinks.map((link, index) => {
+          const isActive =
+            (location.pathname.includes(link.route) && link.route.length > 1) || location.pathname == link.route
+
+          return (
+            <Link to={link.route} key={index} className={`bottombar_link ${isActive && 'bg-primary-500'}`}>
+              <img src={link.imgURL} alt='link' width={16} height={16} className='object-contain' />
+              <p className='text-subtle-medium text-light-1 max-sm:hidden'>{link.label.split(/\s+/)[0]}</p>
+            </Link>
+          )
+        })}
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
