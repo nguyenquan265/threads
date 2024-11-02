63d7d67e7819b85866c406e1c1f908078c5b7508
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-11-02 14:37:59 +0700

    prepare for deploy

--- server/app.ts
+++ server/app.ts
@@ -5,8 +5,10 @@ import cookieParser from 'cookie-parser'
 import ApiError from './utils/ApiError'
 import errorHandler from './middlewares/error.middleware'
 import router from './routes'
+import path from 'path'
 
 const app = express()
+const dirname = path.resolve()
 
 app.use(cors())
 app.use(express.json())
@@ -21,9 +23,18 @@ app.get('/health', (req: Request, res: Response) => {
 //   res.send('This is a protected route')
 // })
 app.use('/api/v1', router)
-app.use('/*', (req: Request, res: Response, next: NextFunction) => {
+app.use('/api/*', (req: Request, res: Response, next: NextFunction) => {
   next(new ApiError(404, `Can't find ${req.originalUrl} on this server!`))
 })
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(dirname, '/client/dist')))
+
+  app.get('*', (req: Request, res: Response) => {
+    res.sendFile(path.resolve(dirname, 'client', 'dist', 'index.html'))
+  })
+}
+
 app.use(errorHandler)
 
 export default app
