cce5b05fdd49c7edeed8c087de819d9f52e093ea
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-11-02 16:32:09 +0700

    temp middleware before update

--- server/middlewares/upload.middleware.ts
+++ server/middlewares/upload.middleware.ts
@@ -1,8 +1,1 @@
-import { uploadRouter } from '@/config/uploadthing'
-import { createRouteHandler } from 'uploadthing/express'
 
-const uploadHandler = createRouteHandler({
-  router: uploadRouter
-})
-
-export default uploadHandler
