7f80205a1e319cee0ea684f803c3bfb33eb50494
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-11-04 16:29:45 +0700

    update auth

--- server/middlewares/auth.middleware.ts
+++ server/middlewares/auth.middleware.ts
@@ -1,26 +1,21 @@
 import ApiError from '@/utils/ApiError'
-import asyncHandler from '@/utils/asyncHandler'
 import { NextFunction, Request, Response } from 'express'
 
-export const authenticate = asyncHandler((req: Request, res: Response, next: NextFunction) => {
-  if (!req.auth?.userId) {
-    throw new ApiError(401, 'You must be logged in to access this resource')
+export const authenticate = (req: Request, res: Response, next: NextFunction) => {
+  if (!req.auth || !req.auth.userId) {
+    next(new ApiError(401, 'You must be logged in to access this resource'))
   }
 
-  return next()
-})
+  next()
+}
 
 export const restrictTo = (...roles: string[]) => {
   return (req: Request, res: Response, next: NextFunction) => {
-    const auth = req.auth
-
-    if (auth) {
-      roles.forEach((role) => {
-        if (!auth.has({ permission: `org:${role}` })) {
-          throw new ApiError(403, 'You do not have permission to access this resource')
-        }
-      })
-    }
+    roles.forEach((role) => {
+      if (req.auth && !req.auth.has({ permission: `org:${role}` })) {
+        next(new ApiError(403, 'You do not have permission to access this resource'))
+      }
+    })
 
     next()
   }
