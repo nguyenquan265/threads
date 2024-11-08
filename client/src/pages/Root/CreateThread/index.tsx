aa30f8d4740d4449f9cf7e045332d125ff99c51f
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-11-08 17:21:59 +0700

    optimization code

--- client/src/pages/Root/CreateThread/index.tsx
+++ client/src/pages/Root/CreateThread/index.tsx
@@ -1,23 +1,13 @@
 import { useGetUser } from '@/apis/UserApi'
 import PostThreadForm from '@/components/forms/PostThreadForm'
 import Loader from '@/components/shared/Loader'
-import { useUser } from '@clerk/clerk-react'
 import { Navigate } from 'react-router-dom'
 
 const CreateThreadPage = () => {
-  const { user, isLoaded, isSignedIn } = useUser()
   const { data: userInfo, isLoading } = useGetUser()
 
-  if (!isLoaded || isLoading) {
-    return (
-      <div className='bg-dark-1 min-h-screen w-full flex items-center justify-center'>
-        <Loader />
-      </div>
-    )
-  }
-
-  if (!isSignedIn || !user) {
-    return <Navigate to='/sign-in' />
+  if (isLoading) {
+    return <Loader />
   }
 
   if (!userInfo?.onboarded) {
--- client/src/pages/Root/Home/index.tsx
+++ client/src/pages/Root/Home/index.tsx
@@ -1,22 +1,16 @@
 import { useGetUser } from '@/apis/UserApi'
 import Loader from '@/components/shared/Loader'
-import { useUser } from '@clerk/clerk-react'
 import { Navigate } from 'react-router-dom'
 
 const Home = () => {
-  const { user, isLoaded, isSignedIn } = useUser()
   const { data: userInfo, isLoading } = useGetUser()
 
-  if (!isLoaded || isLoading) {
-    return (
-      <div className='bg-dark-1 min-h-screen w-full flex items-center justify-center'>
-        <Loader />
-      </div>
-    )
+  if (isLoading) {
+    return <Loader />
   }
 
-  if (!isSignedIn || !user) {
-    return <Navigate to='/sign-in' />
+  if (!userInfo?.onboarded) {
+    return <Navigate to='/onboarding' />
   }
 
   return (
