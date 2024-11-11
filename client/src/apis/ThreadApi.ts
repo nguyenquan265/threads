13cb52a83dc1af523fd6ea71dc58a0717df88d86
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-11-11 16:08:21 +0700

    base thread card setup

--- client/src/apis/ThreadApi.ts
+++ client/src/apis/ThreadApi.ts
@@ -1,11 +1,38 @@
 import { PostThreadData } from '@/components/forms/PostThreadForm'
+import { Thread } from '@/type'
 import { useAuth } from '@clerk/clerk-react'
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
+type GetPostsResponse = {
+  posts: Thread[]
+  isNext: boolean
+}
+
+export const useGetPosts = (page?: number, limit?: number) => {
+  const getPostsRequest = async (): Promise<GetPostsResponse> => {
+    const res = await fetch(`${API_BASE_URL}/api/v1/threads?page=${page || 1}&limit=${limit || 20}`)
+
+    if (!res.ok) {
+      throw new Error('Failed to get posts.')
+    }
+
+    return res.json()
+  }
+
+  const { data, isLoading } = useQuery({
+    queryKey: ['posts', page, limit],
+    queryFn: getPostsRequest,
+    refetchOnWindowFocus: true
+  })
+
+  return { data, isLoading }
+}
+
 export const usePostThread = () => {
   const { getToken } = useAuth()
+  const queryClient = useQueryClient()
 
   const createPostThreadRequest = async (threadData: PostThreadData) => {
     const token = await getToken()
@@ -28,7 +55,10 @@ export const usePostThread = () => {
   }
 
   const { mutateAsync: postThread, isPending } = useMutation({
-    mutationFn: createPostThreadRequest
+    mutationFn: createPostThreadRequest,
+    onSuccess: () => {
+      queryClient.invalidateQueries()
+    }
   })
 
   return { postThread, isPending }
--- client/src/pages/Root/Home/index.tsx
+++ client/src/pages/Root/Home/index.tsx
@@ -1,11 +1,14 @@
+import { useGetPosts } from '@/apis/ThreadApi'
 import { useGetUser } from '@/apis/UserApi'
+import ThreadCard from '@/components/cards/ThreadCard'
 import Loader from '@/components/shared/Loader'
 import { Navigate } from 'react-router-dom'
 
 const Home = () => {
-  const { data: userInfo, isLoading } = useGetUser()
+  const { data: userInfo, isLoading: isUserLoading } = useGetUser()
+  const { data: result, isLoading: isPostsLoading } = useGetPosts()
 
-  if (isLoading) {
+  if (isUserLoading || isPostsLoading) {
     return <Loader />
   }
 
@@ -16,6 +19,28 @@ const Home = () => {
   return (
     <>
       <h1 className='head-text text-left'>Home</h1>
+
+      <section className='mt-9 flex flex-col gap-10'>
+        {result?.posts.length === 0 ? (
+          <p className='no-result'>No threads found</p>
+        ) : (
+          <>
+            {result?.posts.map((post) => (
+              <ThreadCard
+                key={post._id}
+                id={post._id}
+                currentUserId={userInfo.clerkId}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
     </>
   )
 }
