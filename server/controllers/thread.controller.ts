dc993369068f0782d4e2022ffc53d2f7af216ba3
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-11-11 15:45:10 +0700

    get posts

--- server/controllers/thread.controller.ts
+++ server/controllers/thread.controller.ts
@@ -4,6 +4,42 @@ import ApiError from '../utils/ApiError'
 import asyncHandler from '../utils/asyncHandler'
 import { NextFunction, Request, Response } from 'express'
 
+interface GetThreadsRequest extends Request {
+  query: {
+    page: string
+    limit: string
+  }
+}
+
+export const getPosts = asyncHandler(async (req: GetThreadsRequest, res: Response, next: NextFunction) => {
+  const page = parseInt(req.query.page) || 1
+  const limit = parseInt(req.query.limit) || 20
+  const skip = (page - 1) * limit
+
+  // Get all top-level threads
+  const query = Thread.find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: -1 })
+    .skip(skip)
+    .limit(limit)
+    .populate({ path: 'author', model: User })
+    .populate({
+      path: 'children',
+      populate: {
+        path: 'author',
+        model: User,
+        select: '_id name parentId image'
+      }
+    })
+
+  const [totalPosts, posts] = await Promise.all([
+    Thread.countDocuments({ parentId: { $in: [null, undefined] } }),
+    query
+  ])
+  const isNext = totalPosts > skip + posts.length
+
+  res.status(200).json({ posts, isNext })
+})
+
 interface CreateThreadRequest extends Request {
   body: {
     text: string
--- server/routes/thread.routes.ts
+++ server/routes/thread.routes.ts
@@ -1,10 +1,11 @@
 import { clerkMiddleware } from '@clerk/express'
 import { Router } from 'express'
 import { authenticate } from '../middlewares/auth.middleware'
-import { createThread } from '../controllers/thread.controller'
+import { createThread, getPosts } from '../controllers/thread.controller'
 
 const router = Router()
 
+router.get('/', getPosts)
 router.post('/', clerkMiddleware(), authenticate, createThread)
 
 export default router
