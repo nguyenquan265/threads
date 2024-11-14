6ab9da9555ee3f2718b66128a3478dbfc9bcacbe
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-11-14 14:24:20 +0700

    get users api

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -4,6 +4,7 @@ import { hasResourcePermission } from '../middlewares/auth.middleware'
 import ApiError from '../utils/ApiError'
 import User from '../models/user.model'
 import Thread from '../models/thread.model'
+import { FilterQuery, SortOrder } from 'mongoose'
 
 interface UpdateUserRequest extends Request {
   body: {
@@ -72,4 +73,48 @@ export const getUserPosts = asyncHandler(async (req: Request, res: Response, nex
   }
 
   res.status(200).json(userPosts)
+})
+
+interface GetUserRequest extends Request {
+  query: {
+    userId: string
+    page?: string
+    limit?: string
+    searchString?: string
+    sortBy?: string
+  }
+}
+
+export const getUsers = asyncHandler(async (req: GetUserRequest, res: Response, next: NextFunction) => {
+  const { userId, page, limit, searchString, sortBy } = req.query
+  const skip = ((page ? parseInt(page) : 1) - 1) * (limit ? parseInt(limit) : 20)
+
+  const regex = new RegExp(searchString || '', 'i')
+
+  const query: FilterQuery<typeof User> = {
+    clerkId: { $ne: userId }
+  }
+
+  if (searchString?.trim() !== '') {
+    query.$or = [
+      {
+        username: { $regex: regex }
+      },
+      {
+        name: { $regex: regex }
+      }
+    ]
+  }
+
+  const [totalUsers, users] = await Promise.all([
+    User.countDocuments(query),
+    User.find(query)
+      .sort({ createdAt: sortBy ? (sortBy as SortOrder) : 'desc' })
+      .skip(skip)
+      .limit(limit ? parseInt(limit) : 20)
+  ])
+
+  const isNext = totalUsers > skip + users.length
+
+  res.status(200).json({ users, isNext })
 })
--- server/routes/user.routes.ts
+++ server/routes/user.routes.ts
@@ -1,10 +1,11 @@
 import { clerkMiddleware } from '@clerk/express'
 import { Router } from 'express'
-import { getUser, getUserPosts, updateUser } from '../controllers/user.controller'
+import { getUser, getUserPosts, getUsers, updateUser } from '../controllers/user.controller'
 import { authenticate } from '../middlewares/auth.middleware'
 
 const router = Router()
 
+router.get('/', getUsers)
 router.get('/:clerkId', getUser)
 router.patch('/:clerkId', clerkMiddleware(), authenticate, updateUser)
 router.get('/:clerkId/posts', getUserPosts)
