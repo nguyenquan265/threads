c4d6ec3af3aa484d2f3991a200513022041f8f28
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-11-17 19:22:48 +0700

    update ProfileHeader

--- client/src/components/shared/ProfileHeader.tsx
+++ client/src/components/shared/ProfileHeader.tsx
@@ -1,3 +1,6 @@
+import { Link } from 'react-router-dom'
+import EditLogo from '@/assets/edit.svg'
+
 type Props = {
   accountId: string
   currentUserId: string
@@ -22,9 +25,18 @@ const ProfileHeader = ({ accountId, currentUserId, bio, imgUrl, name, username, 
             <p className='text-base-medium text-gray-1'>@{username}</p>
           </div>
         </div>
+
+        {accountId === currentUserId && type !== 'Community' && (
+          <Link to='/profile/edit'>
+            <div className='flex cursor-pointer gap-3 rounded-lg bg-dark-3 px-4 py-2'>
+              <img src={EditLogo} alt='logout' width={16} height={16} />
+
+              <p className='text-light-2 max-sm:hidden'>Edit</p>
+            </div>
+          </Link>
+        )}
       </div>
 
-      {/* TODO: Community */}
       <p className='mt-6 max-w-lg text-base-regular text-light-2'>{bio}</p>
 
       <div className='mt-12 h-0.5 w-full bg-dark-3' />
