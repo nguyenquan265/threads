1fbd971ac077fd63868b82b818f1cabd7dab253f
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-11-18 14:13:38 +0700

    community hook api

---
+++ client/src/apis/CommunityApi.ts
@@ -0,0 +1,73 @@
+import { Community } from '@/type'
+import { useQuery } from '@tanstack/react-query'
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+
+export const useGetCommunityDetails = (communityClerkId?: string) => {
+  const createGetCommunityDetailsRequest = async (): Promise<Community> => {
+    const res = await fetch(`${API_BASE_URL}/api/v1/communities/${communityClerkId}`)
+
+    if (!res.ok) {
+      throw new Error('Failed to get community details')
+    }
+
+    return res.json()
+  }
+
+  const { data, isLoading } = useQuery({
+    queryKey: ['communityDetails', { clerkId: communityClerkId }],
+    queryFn: createGetCommunityDetailsRequest,
+    enabled: !!communityClerkId,
+    staleTime: 0
+  })
+
+  return { data, isLoading }
+}
+
+export const useGetCommunityPosts = (communityClerkId?: string) => {
+  const createGetCommunityPostsRequest = async (): Promise<Community> => {
+    const res = await fetch(`${API_BASE_URL}/api/v1/communities/${communityClerkId}/posts`)
+
+    if (!res.ok) {
+      throw new Error('Failed to get community posts')
+    }
+
+    return res.json()
+  }
+
+  const { data, isLoading } = useQuery({
+    queryKey: ['communityPosts', { clerkId: communityClerkId }],
+    queryFn: createGetCommunityPostsRequest,
+    enabled: !!communityClerkId,
+    staleTime: 0
+  })
+
+  return { data, isLoading }
+}
+
+export const useGetCommunities = (
+  searchString: string = '',
+  page: number = 1,
+  limit: number = 20,
+  sortby: string = 'desc'
+) => {
+  const createGetCommunitiesRequest = async (): Promise<Community[]> => {
+    const res = await fetch(
+      `${API_BASE_URL}/api/v1/communities?page=${page}&limit=${limit}&searchString=${searchString}&sortby=${sortby}`
+    )
+
+    if (!res.ok) {
+      throw new Error('Failed to get communities')
+    }
+
+    return res.json()
+  }
+
+  const { data, isLoading } = useQuery({
+    queryKey: ['communities', { page }, { limit }, { searchString }, { sortby }],
+    queryFn: createGetCommunitiesRequest,
+    staleTime: 0
+  })
+
+  return { data, isLoading }
+}
