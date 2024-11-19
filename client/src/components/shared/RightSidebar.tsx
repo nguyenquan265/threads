8c2c95c88a92350afe151f915b1acc8ed7ac4516
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-11-19 12:54:01 +0700

    complete rightsidebar

--- client/src/components/shared/RightSidebar.tsx
+++ client/src/components/shared/RightSidebar.tsx
@@ -1,11 +1,61 @@
+import { useGetCommunities } from '@/apis/CommunityApi'
+import { useGetUsers } from '@/apis/UserApi'
+import UserCard from '../cards/UserCard'
+import SearchSkeleton from './SearchSkeleton'
+
 const RightSidebar = () => {
+  const { data: usersResult, isLoading: isGetUsersLoading } = useGetUsers(1, 20, '', 'desc')
+  const { data: communitiesResult, isLoading: isGetCommunitiesLoading } = useGetCommunities(1, 20, '', 'desc')
+
   return (
     <section className='custom-scrollbar rightsidebar'>
       <div className='flex flex-1 flex-col justify-start'>
         <h3 className='text-heading4-medium text-light-1'>Suggested communities</h3>
+
+        <div className='mt-7 flex w-[350px] flex-col gap-9'>
+          {isGetCommunitiesLoading && <SearchSkeleton />}
+
+          {communitiesResult && communitiesResult.communities.length > 0 ? (
+            <>
+              {communitiesResult.communities.map((community) => (
+                <UserCard
+                  key={community._id}
+                  id={community.clerkId}
+                  name={community.name}
+                  username={community.username}
+                  imgUrl={community.image}
+                  personType='Community'
+                />
+              ))}
+            </>
+          ) : (
+            <p className='!text-base-regular text-light-3'>No communities yet</p>
+          )}
+        </div>
       </div>
       <div className='flex flex-1 flex-col justify-start'>
-        <h3 className='text-heading4-medium text-light-1'>Suggested users</h3>
+        <h3 className='text-heading4-medium text-light-1'>Similar Minds</h3>
+
+        <div className='mt-7 flex w-[350px] flex-col gap-10'>
+          {isGetUsersLoading && <SearchSkeleton />}
+
+          {usersResult && usersResult.users.length > 0 ? (
+            <>
+              {usersResult.users.map((person) => (
+                <UserCard
+                  key={person._id}
+                  id={person.clerkId}
+                  name={person.name}
+                  username={person.username}
+                  imgUrl={person.image}
+                  personType='User'
+                />
+              ))}
+            </>
+          ) : (
+            <p className='!text-base-regular text-light-3'>No users yet</p>
+          )}
+        </div>
       </div>
     </section>
   )
